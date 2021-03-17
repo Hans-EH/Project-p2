3264d1fbc6b1bd989bc5fb62becc320d645494f0
fibpi-fbt <thorbensen@gmail.com>
2021-03-17 12:03:13 +0100

    Last Session

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Project-p2
+
+## MongoDB documentation
+
+- https://docs.mongodb.com/manual/administration/install-community/
+- https://docs.mongodb.com/drivers/node/fundamentals/crud/write-operations/insert/
+
+## Mac OS
+
+- start db server: brew services start mongodb-community@4.4
+- stop db server: brew services stop mongodb-community@4.4
+- check process: brew services list
--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 const { parse } = require("url");
-const { MongoClient } = require("../node_modules/mongodb");
+const { MongoClient } = require("mongodb");
 const { Device, Appliance } = require("./_devices");
+
+// DB settings
+const uri = "mongodb://localhost:27017/";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+async function checkInsert(props) {
+  try {
+    await client.connect();
+    const database = client.db("app-devices");
+    const device_collection = database.collection("devices");
+    const query = { UUID: props.UUID };
+    const result = await device_collection.findOne(query);
+
+    // If device isn't in the collection add it
+    if (result == null) {
+      // Convert Object to JSON string
+      let json_format = JSON.stringify(props);
+
+      // Insert JSON data in to collection
+      const ins_res = await device_collection.insertOne(props);
+      console.dir(ins_res);
+    }
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
 
 let iphone_12 = new Device("iphone_12");
 console.table(iphone_12);
+//checkInsert(iphone_12).catch(console.dir);
 
 let refrig = new Appliance("Refrigirator", 1240);
 console.table(refrig);
+//checkInsert(refrig).catch(console.dir);
