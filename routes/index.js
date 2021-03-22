d735ede000d226e54553b3ba2febb3b98e0cff21
fibpi-fbt <thorbensen@gmail.com>
2021-03-22 10:40:38 +0100

    Express app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// Demo route
+router.get("/hello", function (req, res, next) {
+  res.render("hello", { title: "Hello Word" });
+});
+
+// User registration route
+router.get("/register", function (req, res, next) {
+  res.render("register", { title: "register" });
+});
+
+// User authentication route
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "login" });
 });
 
 module.exports = router;
