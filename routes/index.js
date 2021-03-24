ab0c3e59ad9e8933fb5aac3f074f42a68fbcd562
fibpi-fbt <thorbensen@gmail.com>
2021-03-24 12:38:50 +0100

    Frokost

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,36 @@
 var express = require("express");
 var router = express.Router();
 
+authenticated = true;
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  // Cookies that have not been signed
-  console.log("Cookies: ", req.cookies);
+  console.log("Cookies: ", req.cookies); // Cookies that have not been signed
+  console.log("Signed Cookies: ", req.signedCookies); // Cookies that have been signed
 
-  // Cookies that have been signed
-  console.log("Signed Cookies: ", req.signedCookies);
+  if (authenticated) {
+    res.render("index", { title: "Express" });
+  } else {
+    res.redirect("/login");
+  }
+});
 
-  res.render("index", { title: "Express" });
+// Settings route
+router.get("/settings", function (req, res, next) {
+  if (authenticated) {
+    res.render("settings", { title: "Settings" });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+// Devices Route
+router.get("/devices", function (req, res, next) {
+  if (authenticated) {
+    res.render("devices", { title: "Devices" });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 // User registration route
@@ -20,15 +41,6 @@ router.get("/register", function (req, res, next) {
 // User authentication route
 router.get("/login", function (req, res, next) {
   res.render("login", { title: "login" });
-});
-
-// Settings route
-router.get("/settings", function (req, res, next) {
-  res.render("settings", { title: "Settings" });
-});
-
-router.get("/devices", function (req, res, next) {
-  res.render("devices", { title: "Devices" });
 });
 
 module.exports = router;
