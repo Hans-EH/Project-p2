bf74447f7331e5f6b6972b57324d8fa77d5dd611
Mihusu <ming2200@hotmail.com>
2021-03-30 09:34:24 +0200

    update device.js

--- scripts/Device.js
+++ scripts/Device.js
@@ -10,10 +10,12 @@ exports.Device = class Device {
         this.energyUsage = energyUsage;
         this.timeAdded = new Date().toLocaleString();
         this.timeModified = new Date().toLocaleString();
-        this.activeTime = {"00:00": false, "01:00": false, "02:00": false, "03:00": false, "04:00": false, "05:00": false,
-                            "06:00": false, "07:00": false, "08:00": false, "09:00": false,"10:00": false, "11:00": false,
-                            "12:00": false, "13:00": false, "14:00": false, "15:00": false, "16:00": false, "17:00": false,
-                            "18:00": false, "19:00": false, "20:00": false, "21:00": false, "22:00": false, "23:00": false};
+        this.activeTime = {
+            "00:00": false, "01:00": false, "02:00": false, "03:00": false, "04:00": false, "05:00": false,
+            "06:00": false, "07:00": false, "08:00": false, "09:00": false, "10:00": false, "11:00": false,
+            "12:00": false, "13:00": false, "14:00": false, "15:00": false, "16:00": false, "17:00": false,
+            "18:00": false, "19:00": false, "20:00": false, "21:00": false, "22:00": false, "23:00": false
+        };
 
     }
 
@@ -22,11 +24,11 @@ exports.Device = class Device {
      * @param {Object} time An object containing the time slot and a boolean value in this format.
      * e.g {"00:00": true, "01:00": false}
      */
-    updateActiveTime(time){
+    updateActiveTime(time) {
         //Loop over every entry in the time dictionary object
         //adding only valid time strings to the active array
-        for (let key in time){
-            if (key in this.activeTime){
+        for (let key in time) {
+            if (key in this.activeTime) {
                 this.activeTime[key] = time[key];
             }
         }
@@ -36,3 +38,21 @@ exports.Device = class Device {
 
     }
 }
+
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+async function testConnect() {
+    try {
+        await client.connect();
+        const database = client.db("<monitor-project>");
+        console.log("Got to this point")
+
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+testConnect();
