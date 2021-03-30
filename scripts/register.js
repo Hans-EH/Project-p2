00138f78dca11823de7979baa79d09d8d74d7747
fibpi-fbt <thorbensen@gmail.com>
2021-03-30 09:37:24 +0200

    Update register.js

--- scripts/register.js
+++ scripts/register.js
@@ -7,30 +7,45 @@ async function testConnect() {
   try {
     await client.connect();
     const database = client.db("<monitor-project>");
-    console.log("Got to this point")
     const table = database.collection("users");
-    const query = { };
-    const documentList = await table.find({}).toArray();
-    //Test profiling
     const profile_table = database.collection("profiles");
-
+    const documentList = await table.find({}).toArray();
 
     //shows all registered users
-    console.log("All Registered Users: "+documentList.length+"\n");
-    for(let i = 0; i < documentList.length; i++){
+    console.log("All Registered Users: " + documentList.length + "\n");
+    for (let i = 0; i < documentList.length; i++) {
       const profile_link = documentList[i]._id.toString();
-      const profile_query = {user_link : profile_link};
+      const profile_query = { user_link: profile_link };
       const profile = await profile_table.findOne(profile_query);
-      let j = i+1;
-      console.log("User "+j+":");
+      let j = i + 1;
+      console.log("User " + j + ":");
       console.log(documentList[i]);
       console.log(profile);
     }
-
+    // shows all users end
     return documentList;
   } catch (e) {
     console.error(e);
   }
-};
+}
+
+async function registerUser() {
+  try {
+    await client.connect();
+    const db = client.db("<monitor-project>");
+    const table = db.collection("users");
+    const profile_table = db.collection("profiles");
+
+    const hans = {
+      user_id: "Hans",
+      password: "123456",
+    };
+
+    table.insertOne(hans);
+  } catch (error) {
+    print(error);
+  }
+}
 
 testConnect();
+registerUser();
