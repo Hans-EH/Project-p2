5167caa5d41bc4a79ecb968bd809ce27a1fe3263
fibpi-fbt <thorbensen@gmail.com>
2021-03-30 10:55:11 +0200

    New changes

--- scripts/register.js
+++ scripts/register.js
@@ -27,28 +27,7 @@ async function testConnect() {
   } catch (e) {
     console.error(e);
   }
-};
-
-// validates that all register information is correct
-async function validateRegInfo() {
-  const express = require("express");
-  const bodyParser = require("body-parser");
-  const router = express.Router();
-  const app = express();
-  
-  //Here we are configuring express to use body-parser as middle-ware.
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  
-  router.post('/handle',(request,response) => {
-      //code to perform particular action.
-      //To access POST variable use req.body()methods.
-      console.log(request.body);
-  });
-  
-  // add router in the Express app.
-  app.use("/", router);
-};
+}
 
 async function registerUser(UserObject) {
   try {
